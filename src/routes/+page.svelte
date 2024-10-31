559c2f8be4db8a680c972cd24ff879f53382f6fc
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-10-31 18:27:47 +0200

    add BMI calculator page and update main navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <main class="ext-center container mx-auto mt-12 h-screen max-w-xl space-y-8">
 	<h1 class="mb-6 text-xl font-bold">Welcome to SvelteKit challenges</h1>
+	<p class="font-semibold">
+		Created by Eetu Rönkkö <span
+			><a class="font-semibold underline" href="https://github.com/eeturonkko" target="”_blank”"
+				>Github</a
+			></span
+		>
+	</p>
+
 	<div class="grid grid-cols-3 place-content-center gap-4">
 		<a class="rounded-md bg-green-500 text-center font-semibold text-white" href="/counter"
 			>Counter</a
@@ -15,6 +23,9 @@
 			class="rounded-md bg-purple-500 text-center font-semibold text-white"
 			href="/password-generator
     ">Password Generator</a
+		>
+		<a class="rounded-md bg-red-500 text-center font-semibold text-white" href="/bmi-calculator"
+			>BMI Calculator</a
 		>
 	</div>
 </main>
---
+++ src/routes/bmi-calculator/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	let weight = $state<number>();
+	let height = $state<number>();
+	let bmi = $state<number>(0);
+
+	function calculateBMI() {
+		if (!weight || !height) {
+			return;
+		}
+		return (bmi = weight / ((height / 100) * (height / 100)));
+	}
+</script>
+
+<main class="h-screen">
+	<section
+		class="container mx-auto mt-24 max-w-xl rounded-md border border-black bg-gray-200 p-4 drop-shadow-md"
+	>
+		<div class="max-w- flex flex-col border">
+			<h2 class="mb-2 font-semibold">BMI Calculator</h2>
+			<div class="mb-4 flex flex-col gap-1">
+				<label class="font-semibold" for="">Weight (kg)</label>
+				<input bind:value={weight} type="text" class="max-w-xs" />
+				<label class="font-semibold" for="">Height (cm)</label>
+				<input bind:value={height} type="text" class="max-w-xs" />
+				<button
+					class="px-58 mt-4 max-w-32 text-nowrap rounded bg-white font-semibold"
+					onclick={calculateBMI}>Calculate BMI</button
+				>
+			</div>
+			<div class="mb-2">
+				{#if bmi > 0}
+					<p class="font-semibold">Your BMI is: {bmi.toFixed(1)}</p>
+				{/if}
+			</div>
+			<a class="max-w-16 rounded bg-white p-2 font-semibold text-black" href="/">Home</a>
+		</div>
+	</section>
+</main>
