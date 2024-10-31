f252d9341a887c4a2c925ad0ad3ef60761d3ea69
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-10-31 16:29:04 +0200

    implement temperature conversion logic in the temperature converter page

--- src/routes/temp-converter/+page.svelte
+++ src/routes/temp-converter/+page.svelte
@@ -1,4 +1,28 @@
 <script lang="ts">
+	class Temperature {
+		#c = $state<number>();
+		#f = $state<number>();
+
+		get c() {
+			return this.#c;
+		}
+
+		set c(c) {
+			this.#c = Math.ceil(c!!);
+			this.#f = Math.ceil(c!! * (9 / 5) + 32);
+		}
+
+		get f() {
+			return this.#f;
+		}
+
+		set f(f) {
+			this.#f = f;
+			this.#c = (f!! - 32) * (5 / 9);
+		}
+	}
+
+	const temperature = new Temperature();
 </script>
 
 <main class="h-screen">
@@ -7,8 +31,26 @@
 	>
 		<div class="max-w- flex flex-col border">
 			<h2 class="font-semibold">Temperature Converter</h2>
+			<div class="my-10 flex gap-4 text-nowrap drop-shadow-xl">
+				<input
+					bind:value={temperature.c}
+					class="max-w-32 rounded border"
+					name="celcius"
+					id="celcius"
+					type="text"
+				/>
+				<label class="font-semibold" for="celcius">Celsius =</label>
+				<input
+					bind:value={temperature.f}
+					class="max-w-32 rounded border"
+					name="fahrenheit"
+					id="fahrenheit"
+					type="text"
+				/>
+				<label class="font-semibold" for="celcius">Fahrenheit</label>
+			</div>
 
-			<a class="rounded bg-white p-2 font-semibold text-black" href="/">Home</a>
+			<a class="max-w-16 rounded bg-white p-2 font-semibold text-black" href="/">Home</a>
 		</div>
 	</section>
 </main>
